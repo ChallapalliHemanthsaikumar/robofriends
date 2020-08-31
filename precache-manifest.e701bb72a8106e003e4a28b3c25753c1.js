bc4f98cceee9e5f4d4f4cf3475b746d05fcd043b
ChallapalliHemanthsaikumar <67137724+ChallapalliHemanthsaikumar@users.noreply.github.com>
2020-08-31 14:19:32 +0530

    Updates

---
+++ precache-manifest.e701bb72a8106e003e4a28b3c25753c1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbeac519db32d80d3d850abe4a6354b3",
+    "url": "/ChallapalliHemanthsaikumar/robofriends/index.html"
+  },
+  {
+    "revision": "939c29a12718c22b5acf",
+    "url": "/ChallapalliHemanthsaikumar/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "d48dbdcaf5d2b8e32896",
+    "url": "/ChallapalliHemanthsaikumar/robofriends/static/css/main.340fbd8c.chunk.css"
+  },
+  {
+    "revision": "939c29a12718c22b5acf",
+    "url": "/ChallapalliHemanthsaikumar/robofriends/static/js/2.e7e24cc0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ChallapalliHemanthsaikumar/robofriends/static/js/2.e7e24cc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d48dbdcaf5d2b8e32896",
+    "url": "/ChallapalliHemanthsaikumar/robofriends/static/js/main.6e5d444b.chunk.js"
+  },
+  {
+    "revision": "8fafff994c8a507df1cb",
+    "url": "/ChallapalliHemanthsaikumar/robofriends/static/js/runtime-main.337cd06b.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/ChallapalliHemanthsaikumar/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
